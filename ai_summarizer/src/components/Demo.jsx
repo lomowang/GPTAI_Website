50a1d866e642b6ab076c99695ea9c7029bb61751
lomowang <frees1102@gmail.com>
2023-04-23 02:39:49 +0800

    202304223

---
+++ ai_summarizer/src/components/Demo.jsx
@@ -0,0 +1,61 @@
+import { useState, useEffect } from "react";
+import { copy, linkIcon , loader , tick} from '../assets';
+
+
+const Demo = () => {
+
+    // 調用文章狀態
+  const [article, setArticle] = useState({
+    url:'',
+    summary:'',
+  })
+
+    const handleSubit = async() =>{
+      alert('Submitted');
+    }
+
+
+return(
+  <section className="mt-16 w-full max-w-xl">
+    {/* { Search} */}
+    <div className="flex flex-col w-full">
+      <form 
+      className="relative flex justify-center items-center"
+      onSubmit={handleSubit}
+      >
+        <img src={linkIcon} 
+        alt="link_icon" 
+        className="absolute left-0 my-2 ml-3 w-5"
+        />
+
+        <input 
+          type="url" 
+          placeholder="Enter a URL"
+          value={article.url}
+          onChange={(e)=> setArticle({...article, url:e.target.value})}
+          required
+          className="url_input peer"
+          />
+
+          <button
+          type="submit"
+          className="submit_btn"
+          peer-focus:border-gray-700
+          peer-focus:text-gray-700
+          >
+            <p>↵</p>
+          </button>
+
+      </form>
+      {/* { Browser URL History} */}
+
+    </div>
+
+    {/* { Display Results} */}
+  </section>
+)
+
+
+};
+
+export default Demo;
